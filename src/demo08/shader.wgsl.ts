92d0beb02af0efdce294a2c2280745ec340afffa
tama <tama@ttk1.net>
2023-06-26 23:34:48 +0900

    texture_2d_array で複数テクスチャ切り替え版

---
+++ src/demo08/shader.wgsl.ts
@@ -0,0 +1,28 @@
+export default `
+struct VertexOut {
+  @builtin(position) position : vec4f,
+  @location(0) uv : vec2f
+}
+
+@group(0) @binding(0) var<uniform> mvpMat: mat4x4f;
+
+@vertex
+fn vertex_main(@location(0) position: vec3f,
+               @location(1) uv: vec2f) -> VertexOut
+{
+  var output : VertexOut;
+  output.position = mvpMat * vec4(position, 1.0);
+  output.uv = uv;
+  return output;
+}
+
+@group(0) @binding(1) var mySampler: sampler;
+@group(0) @binding(2) var myTexture: texture_2d_array<f32>;
+@group(0) @binding(3) var<uniform> textureId: u32;
+
+@fragment
+fn fragment_main(fragData: VertexOut) -> @location(0) vec4f
+{
+  return textureSample(myTexture, mySampler, fragData.uv, textureId);
+}
+`;
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import demo04 from './demo04/main';
 import demo05 from './demo05/main';
 import demo06 from './demo06/main';
 import demo07 from './demo07/main';
+import demo08 from './demo08/main';
 
 const pages = {
   basic_render_pipeline: basic_render_pipeline,
@@ -18,6 +19,7 @@ const pages = {
   demo05: demo05,
   demo06: demo06,
   demo07: demo07,
+  demo08: demo08,
 };
 
 function index() {
